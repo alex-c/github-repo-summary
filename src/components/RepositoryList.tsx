fc6c7619ae18fb58c37138c142ccbcab41343dca
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-16 20:35:37 +0200

    Added a table view for the repository list

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,19 +1,65 @@
-import React from 'react';
-import { Card, Elevation } from '@blueprintjs/core';
+import React, { useState } from 'react';
+import { Card, Elevation, Button, HTMLTable } from '@blueprintjs/core';
 import { Repository } from '../models/Repository';
 import RepositoryView from './RepositoryView';
+import { IconNames } from '@blueprintjs/icons';
 
 function RepositoryList(props: any) {
   const repositories: Repository[] = props.repositories;
+
+  const [viewMode, setViewMode] = useState('tiles');
+
+  const toggleViewMode = () => {
+    viewMode === 'tiles' ? setViewMode('table') : setViewMode('tiles');
+  };
+
   return (
     <div>
       {repositories.length > 0 ? (
         <Card id="repository-list" elevation={Elevation.TWO}>
-          <b>All Repositories</b>
-          <div>
-            {repositories.map((repository, index) => (
-              <RepositoryView repository={repository} key={index} />
-            ))}
+          <div id="repository-list-header">
+            <div id="repository-list-controls">
+              <Button
+                text={viewMode === 'tiles' ? 'View table' : 'View tiles'}
+                onClick={toggleViewMode}
+                icon={viewMode === 'tiles' ? IconNames.TH : IconNames.GRID_VIEW}
+              />
+            </div>
+            <h2>Repositories</h2>
+          </div>
+          <div id="repository-list-content">
+            {viewMode === 'tiles' ? (
+              repositories.map((repository, index) => <RepositoryView repository={repository} key={index} />)
+            ) : (
+              <HTMLTable bordered={true} striped={true}>
+                <thead>
+                  <tr>
+                    <th>Repository</th>
+                    <th>Description</th>
+                    <th>Language</th>
+                    <th>Stars</th>
+                    <th>Watchers</th>
+                    <th>Forked</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {repositories.map((repository, index) => (
+                    <tr key={index}>
+                      <td>
+                        <a href={repository.html_url} target="_blank" rel="noopener noreferrer">
+                          {repository.name}
+                        </a>
+                      </td>
+                      <td>{repository.description}</td>
+                      <td>{repository.language}</td>
+                      <td>{repository.stargazers_count}</td>
+                      <td>{repository.watchers_count}</td>
+                      <td>{repository.forks}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </HTMLTable>
+            )}
           </div>
         </Card>
       ) : (
