a9851e7f857d5d6b792f5e2020be036b8489d44d
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-16 21:36:19 +0200

    Added build to /docs for Github pages

---
+++ docs/precache-manifest.829208568bb4e7477742a636a9f67bfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eb87c3ee1f5cee50baece4f304c9f00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e9ab39fecca493d0a7c",
+    "url": "/static/css/2.14852a02.chunk.css"
+  },
+  {
+    "revision": "b4bcfd5f6c5b7462bf4e",
+    "url": "/static/css/main.72457038.chunk.css"
+  },
+  {
+    "revision": "0e9ab39fecca493d0a7c",
+    "url": "/static/js/2.2574342b.chunk.js"
+  },
+  {
+    "revision": "b4bcfd5f6c5b7462bf4e",
+    "url": "/static/js/main.bdac6b60.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
