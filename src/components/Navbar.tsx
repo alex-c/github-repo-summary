16ea37445308dc2e0db1b53757d0b08d07097534
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-24 13:31:23 +0200

    Removed obsolete popover from search bar and fixed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,5 @@
 import React, { useState } from 'react';
-import {
-  Popover,
-  Card,
-  PopoverPosition,
-  InputGroup,
-  Icon,
-  Intent,
-  Colors,
-  Tooltip,
-  Button,
-  AnchorButton,
-} from '@blueprintjs/core';
+import { InputGroup, Intent, Tooltip, Button, AnchorButton } from '@blueprintjs/core';
 import { IconNames } from '@blueprintjs/icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppState } from '../models/AppState';
@@ -19,7 +8,6 @@ import { searchUser } from '../actions/thunkActionCreators';
 function Navbar() {
   const dispatch = useDispatch();
   const [userName, setUserName] = useState('');
-  const [popoverVisible, setPopoverVisible] = useState(false);
   const currentUserName = useSelector((state: AppState) => state.user.login);
 
   const searchHandler = () => {
@@ -37,34 +25,26 @@ function Navbar() {
       <div className="viewport">
         <div id="navbar-title">Github Repo Summary</div>
         <div>
-          <Popover position={PopoverPosition.BOTTOM} isOpen={popoverVisible}>
-            <InputGroup
-              placeholder="Github user name"
-              rightElement={
-                <>
-                  <Tooltip content="Search">
-                    <Button icon={IconNames.SEARCH} intent={Intent.PRIMARY} minimal={true} onClick={searchHandler} />
-                  </Tooltip>
-                  <Tooltip content="Clear">
-                    <Button icon={IconNames.TRASH} intent={Intent.DANGER} minimal={true} onClick={resetHandler} />
-                  </Tooltip>
-                </>
+          <InputGroup
+            placeholder="Github user name"
+            rightElement={
+              <>
+                <Tooltip content="Search">
+                  <Button icon={IconNames.SEARCH} intent={Intent.PRIMARY} minimal={true} onClick={searchHandler} />
+                </Tooltip>
+                <Tooltip content="Clear">
+                  <Button icon={IconNames.TRASH} intent={Intent.DANGER} minimal={true} onClick={resetHandler} />
+                </Tooltip>
+              </>
+            }
+            value={userName}
+            onChange={(event: { target: any }) => setUserName(event.target.value)}
+            onKeyPress={event => {
+              if (event.charCode === 13) {
+                searchHandler();
               }
-              value={userName}
-              onChange={(event: { target: any }) => setUserName(event.target.value)}
-              onKeyPress={event => {
-                if (event.charCode === 13) {
-                  searchHandler();
-                }
-              }}
-            />
-            <Card>
-              <Icon icon={IconNames.ERROR} intent={Intent.DANGER} style={{ float: 'left' }} />
-              <p>
-                User <span style={{ color: Colors.RED1 }}>{userName}</span> could not be found!
-              </p>
-            </Card>
-          </Popover>
+            }}
+          />
         </div>
         <div id="navbar-right">
           <AnchorButton
