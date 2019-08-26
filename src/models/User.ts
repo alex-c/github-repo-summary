34f64f16bffb8bcfddea3496f00bfe6ff1223ed4
Alexandre Charoy <alexandre.charoy@gmail.com>
2019-08-26 15:46:20 +0200

    Fixed profile link

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,7 +7,7 @@ export type User = {
   blog: string,
   email: string,
   avatar_url: string,
-  htmlUrl: string,
+  html_url: string,
   public_repos: number,
   public_gists: number,
   followers: number,
